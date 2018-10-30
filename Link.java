8f391a5702bd2c2639ef3e7a082d8572ab0ab36a
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2018-10-30 16:12:52 +0100

    Add files via upload

--- Link.java
+++ Link.java
@@ -1,7 +1,7 @@
 /**
 * @wi smut_git-511 bsabbadbdsanbhdsadsanbhdsa
 */
-class HelloWorld 
+public class MyClass 
 { 
  // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. @wi smut_git-511 lalalalalala

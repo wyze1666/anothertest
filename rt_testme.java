b5d4b696e97069aeb7e9425a8122d2f8967061bd
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2019-03-18 16:43:04 +0100

    Add files via upload

---
+++ rt_testme.java
@@ -0,0 +1,11 @@
+/**
+* @wi smut_git-514 funguj krame
+*/
+public class MyClass 
+{ 
+ // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. @wi smut_git-514 musis
+public String getAuthor() {
+return this.author;
+} 
+} 

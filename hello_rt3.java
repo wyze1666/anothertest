137dac2b05c3568ac58155188459d3f265e286e3
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2019-07-16 12:43:35 +0200

    Add files via upload
    Jav aaatest

--- hello_rt3.java
+++ hello_rt3.java
@@ -1,10 +1,10 @@
 /**
-* @wi smut_git-514 aaaa
+* @wi smut_git-515 java tests
 */
 public class MyClass 
 { 
  // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. @wi smut_git-514 aaaa
+    // Prints "Hello, World" to the terminal window. @wi smut_git-515 new java test
 public String getAuthor() {
 return this.author;
 } 

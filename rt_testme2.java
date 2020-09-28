d2e76932e30ada0fd27cfc244da6fb72f86ce658
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2020-09-28 16:34:19 +0200

    Add files via upload
    smut_git-517 - test after coma

---
+++ rt_testme2.java
@@ -0,0 +1,11 @@
+/**
+* @wi smut_git-517 - test after coma
+*/
+public class MyClass 
+{ 
+ // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. @wi smut_git-517 - test after coma
+public String getAuthor() {
+return this.author;
+} 
+} 

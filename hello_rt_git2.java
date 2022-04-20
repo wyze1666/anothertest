994367ecfb0da68eb443dd6261acacc5b524330e
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2022-04-20 12:39:03 +0200

    Add files via upload

---
+++ hello_rt_git2.java
@@ -0,0 +1,11 @@
+/**
+* @wi GITH-510 - trest2
+*/
+public class MyClass 
+{ 
+ // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. @wi GITH-510 - trest2
+public String getAuthor() {
+return this.author;
+} 
+} 

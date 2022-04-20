2cb3fa0b25d312f88ac1fcd791c5a124619286a1
wyze1666 <32898124+wyze1666@users.noreply.github.com>
2022-04-20 12:50:40 +0200

    Add files via upload

---
+++ hello_rt_git3.java
@@ -0,0 +1,12 @@
+/**
+* @wi GITH-511 - trest3
+*/
+public class MyClass 
+{ 
+public String getAuthor() {
+	
+	// Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. @wi GITH-511 - trest3
+return this.author;
+} 
+} 
